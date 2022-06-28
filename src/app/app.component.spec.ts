084c68d7bf5c75626db36fe82c4518dca93d0fa9
dtrommel <80407337+dtrommel@users.noreply.github.com>
2022-06-28 15:07:23 +0200

    Added calculation methods and updated test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
@@ -22,10 +23,43 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Calculator');
   });
 
-  it('should render title', () => {
+  it('should add two numbers correctly', ()=>{
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Calculator app is running!');
+    const app = fixture.componentInstance;
+    app.firstAdditionNumber = 10;
+    app.secondAdditionNumber = 20;
+    expect(app.Addition()).toEqual(30);
+  });
+
+  it('should substract two numbers correctly', ()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.firstSubstractionNumber = 10;
+    app.secondSubstractionNumber = 20;
+    expect(app.Substraction()).toEqual(-10);
+  });
+
+  it('should multiply two numbers correctly', ()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.firstMultiplicationNumber = 10;
+    app.secondMultiplicationNumber = 20;
+    expect(app.Multiplication()).toEqual(200);
+  });
+
+  it('should divide two numbers correctly', ()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.firstDivisionNumber = 10;
+    app.secondDivisionNumber = 20;
+    expect(app.Division()).toEqual(0.5);
+  });
+
+  it('should substract two numbers correctly', ()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.firstExponentiationNumber = 10;
+    app.secondExponentiationNumber = 20;
+    expect(app.Exponentiation()).toEqual(100000000000000000000);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+  firstAdditionNumber : number;
+  secondAdditionNumber : number;
+  additionResult : number;
+  firstSubstractionNumber : number;
+  secondSubstractionNumber : number;
+  substractionResult : number;
+  firstMultiplicationNumber : number;
+  secondMultiplicationNumber : number;
+  multiplicationResult : number;
+  firstDivisionNumber : number;
+  secondDivisionNumber : number;
+  divisionResult : number;
+  firstExponentiationNumber : number;
+  secondExponentiationNumber : number;
+  exponentiationResult : number;
+
+  Addition() : number{
+    return this.firstAdditionNumber + this.secondAdditionNumber;
+  }
+
+  Substraction() : number{
+    return this.firstSubstractionNumber - this.secondSubstractionNumber;
+  }
+
+  Multiplication() : number{
+    return this.firstMultiplicationNumber * this.secondMultiplicationNumber;
+  }
+
+  Division() : number{
+    return this.firstDivisionNumber / this.secondDivisionNumber;
+  }
+
+  Exponentiation() : number{
+    return Math.pow(this.firstExponentiationNumber,this.secondExponentiationNumber);
+  }
+
+  isNaN : Function = Number.isNaN;
+
+
+
 }
