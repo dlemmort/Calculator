74591fb6f2633f49c06126c249436abc1896534a
dtrommel <80407337+dtrommel@users.noreply.github.com>
2022-06-28 17:26:06 +0200

    Karma Firefox

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chromium-Browser'],
+    browsers: ['Chrome','Firefox'],
     singleRun: false,
     restartOnFileChange: true
   });
